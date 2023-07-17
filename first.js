4c4e788fd1ab1c607c0f7d679129702fea1efacb
saichand24 <kaluvalasaichand@gmail.com>
2023-07-17 20:38:41 +0530

    Data Types JavaScript

--- first.js
+++ first.js
@@ -1,6 +1,39 @@
-let data = 8      //number
-let user = 'saichand'   //string
+//string
 
-let num1 = 2323262326566561n
+// let firstName = 'sai'
+// let lastName = 'chand'
 
-console.log(num1)
+// let user = firstName + ' ' + lastName  //printing with space
+
+// console.log(user)
+
+// let user = 'saichand klauvala'
+// console.log(user);
+
+// let user = 'saichand "klauvala"'  //printing in double quotes
+// console.log(user);
+
+
+// let user = 'saichand \'klauvala\''  //"\"escape charecter
+// console.log(user);
+
+// let user = "sai\nchand"  //"\n"new line
+// console.log(user);
+
+// let user = "sai\tchand"  //"\t"to use tab
+// console.log(user);
+
+
+//boolean
+
+// let bool = 5 > 6    //false
+// console.log(bool)
+
+// let bool = 5 > 6       
+// console.log(typeof bool)
+
+// let user = null 
+// console.log(user)
+
+// let user             //undefined
+// console.log(user)
