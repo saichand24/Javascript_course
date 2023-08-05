a5b696b3a53887628e687d0b112794030974a340
saichand24 <kaluvalasaichand@gmail.com>
2023-08-05 20:22:05 +0530

    Template Literal in JavaScript

--- first.js
+++ first.js
@@ -414,3 +414,36 @@
 //     default:
 //         console.log("7am")                       // 7am
 // }
+//-------------------------------------------------------------------------------------------------------
+// Template Literal in JavaScript
+
+// let num1 = 7;
+// let num2 = 4;
+// let result = num1 + num2;
+
+// console.log("The addition of 7 and 4 is", result);      //The addition of 7 and 4 is 11
+
+
+// let num1 = 7;
+// let num2 = 4;
+// let result = num1 + num2;
+
+// console.log("The addition of " + num1 + " and " +num2 + " is " + result);        //The addition of 7 and 4 is 11
+
+
+// let num1 = 7;
+// let num2 = 4;
+// let result = num1 + num2;
+
+// console.log(`The addition of ${num1} and ${num2} is ${result}`);        //The addition of 7 and 4 is 11
+
+// let num1 = 7;
+// let num2 = 4;
+// let result = num1 + num2;
+
+// console.log(`The addition of ${num1} and ${num2} is ${result}`);  
+// console.log(`My name is 
+// Saichand`); 
+// The addition of 7 and 4 is 11
+// My name is 
+// Saichand
