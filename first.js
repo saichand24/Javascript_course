0889972dec11b57111a2340aab6064d25c7106de
saichand24 <kaluvalasaichand@gmail.com>
2023-08-09 17:25:43 +0530

    Function Expression in JavaScript

--- first.js
+++ first.js
@@ -624,11 +624,23 @@
 
 //function return and passing
 
-function greet(user)
-{
-    return `Hello ${user}`
-}
+// function greet(user)
+// {
+//     return `Hello ${user}`
+// }
 
-let user = 'Saichand';
-let str = greet(user);
-console.log(str);
+// let user = 'Saichand';
+// let str = greet(user);
+// console.log(str);
+
+//Function Expression in JavaScript
+
+let add = function(num1, num2)
+            {
+                return num1 + num2;
+            }
+let sum = add
+
+let result = sum(5.6)
+
+console.log(sum);
