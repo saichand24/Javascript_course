668aeada24ac3b0d0f8703efbc69ec3a29f27436
saichand24 <kaluvalasaichand@gmail.com>
2023-08-09 17:44:48 +0530

    Arrow function in JavaScript

--- first.js
+++ first.js
@@ -661,10 +661,28 @@
 // let str = greet(user);
 // console.log(str);
 
-function add(num1,num2)
-{
-    return num1 + num2;
+// function add(num1,num2)
+// {
+//     return num1 + num2;
+// }
+
+// let result = add(5,6);
+// console.log(result);
+
+//---------------------------------------------------------------------------------------
+
+//Arrow function in JavaScript
+
+let greet = (user) => {
+    console.log('Hello ' + user);
+    return 1;
 }
 
-let result = add(5,6);
+console.log(greet('Saichand'));
+
+let add = (num1,num2) => num1 + num2;
+
+
+let result = add(5,6)
+
 console.log(result);
